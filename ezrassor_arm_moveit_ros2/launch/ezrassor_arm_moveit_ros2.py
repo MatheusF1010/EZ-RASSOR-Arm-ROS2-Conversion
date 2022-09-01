2f5f853b405a9eee6d8ab3456eaab687869c2a1f
MatheusF1010 <matheuspavini@gmail.com>
2022-08-31 20:30:10 -0400

    Converting xml to python for ROS2

--- ezrassor_arm_moveit_ros2/launch/ezrassor_arm_moveit_ros2.py
+++ ezrassor_arm_moveit_ros2/launch/ezrassor_arm_moveit_ros2.py
@@ -20,6 +20,7 @@ def generate_launch_description():
     debug_launch_arg = DeclareLaunchArgument(
         "debug", default_value=TextSubstitution(text="false")
     )
+
     if LaunchConfiguration("debug") == "true":
         launch_prefix_launch_arg = DeclareLaunchArgument(
             "launch_prefix",
@@ -32,16 +33,16 @@ def generate_launch_description():
         )
     
     info_launch_arg = DeclareLaunchArgument(
-        "info", default_value=debug_launch_arg
+        "info", default_value=LaunchConfiguration("debug")
     )
 
     if LaunchConfiguration("info") == "true":
-        launch_prefix_launch_arg = DeclareLaunchArgument(
+        command_args_launch_arg = DeclareLaunchArgument(
             "command_args",
             default_value='--debug'
         )
     else:
-        launch_prefix_launch_arg = DeclareLaunchArgument(
+        command_args_launch_arg = DeclareLaunchArgument(
             "command_args",
             default_value=''
         )
@@ -87,133 +88,129 @@ def generate_launch_description():
     )
 
     # include another launch file
-    planning_context_launch_include = IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                os.path.join(
-                get_package_share_directory('ezrassor_arm_moveit_ros2'),
-                'launch/planning_context.launch')
-            ),
-            load_robot_description_launch_arg = DeclareLaunchArgument(
-                {"load_robot_description": LaunchConfiguration("load_robot_description")}
-            )
-        )
+    # planning_context_launch_include = IncludeLaunchDescription(
+    #     PythonLaunchDescriptionSource(
+    #         os.path.join(
+    #         get_package_share_directory('ezrassor_arm_moveit_ros2'),
+    #         'launch/planning_context.launch'),
+    #     ),
+    #     launch_arguments=[{
+    #         "load_robot_description": LaunchConfiguration("load_robot_description")
+    #     }]
+    # )
 
-    planning_pipeline_launch_include = GroupAction(
-        actions=[
-            # push-ros-namespace to set namespace of included nodes
-            PushRosNamespace('move_group'),
-            IncludeLaunchDescription(
-                PythonLaunchDescriptionSource(
-                    os.path.join(
-                        get_package_share_directory('ezrassor_arm_moveit_ros2'),
-                        'launch/planning_pipeline.launch.xml')
-                ),
-                pipeline_launch_arg = DeclareLaunchArgument(
-                    {"pipeline": LaunchConfiguration("pipeline")}
-                ),
-                parameters=[{
-                "capabilities": LaunchConfiguration('capabilities'),
-                "disable_capabilities": LaunchConfiguration('disable_capabilities'),
-                }]
-            )
-        ]
-    )
+    # planning_pipeline_launch_include = GroupAction(
+    #     actions=[
+    #         # push-ros-namespace to set namespace of included nodes
+    #         PushRosNamespace('move_group'),
+    #         IncludeLaunchDescription(
+    #             PythonLaunchDescriptionSource(
+    #                 os.path.join(
+    #                     get_package_share_directory('ezrassor_arm_moveit_ros2'),
+    #                     'launch/planning_pipeline.launch.xml')
+    #             ),
+    #             launch_arguments=[{
+    #                 "pipeline": LaunchConfiguration("pipeline"),
+    #                 "capabilities": LaunchConfiguration('capabilities'),
+    #                 "disable_capabilities": LaunchConfiguration('disable_capabilities')
+    #             }],
 
-    if LaunchConfiguration("allow_trajectory_execution") == "true":
-        if LaunchConfiguration("fake_execution") == "true":
-            trajectory_execution_launch_include = GroupAction(
-                actions=[
-                    # push-ros-namespace to set namespace of included nodes
-                    PushRosNamespace('move_group'),
-                    IncludeLaunchDescription(
-                        PythonLaunchDescriptionSource(
-                            os.path.join(
-                                get_package_share_directory('ezrassor_arm_moveit_ros2'),
-                                'launch/sensor_manager.launch.xml')
-                        ),
-                        moveit_manage_controllers_launch_arg = DeclareLaunchArgument(
-                            "moveit_manage_controllers",
-                            default_value="true"
-                        ),
-                        moveit_controller_manager_launch_arg = DeclareLaunchArgument(
-                            "moveit_controller_manager", 
-                            default_value="fake"
-                        ),
-                        execution_type_launch_arg = DeclareLaunchArgument(
-                            {"execution_type": LaunchConfiguration("execution_type")}
-                        )
-                    )
-                ]
-            )
-        else:
-            trajectory_execution_launch_include = GroupAction(
-                actions=[
-                    # push-ros-namespace to set namespace of included nodes
-                    PushRosNamespace('move_group'),
-                    IncludeLaunchDescription(
-                        PythonLaunchDescriptionSource(
-                            os.path.join(
-                                get_package_share_directory('ezrassor_arm_moveit_ros2'),
-                                'launch/sensor_manager.launch.xml')
-                        ),
-                        moveit_manage_controllers_launch_arg = DeclareLaunchArgument(
-                            "moveit_manage_controllers",
-                            default_value="true"
-                        ),
-                        moveit_controller_manager_launch_arg = DeclareLaunchArgument(
-                            "moveit_controller_manager", 
-                            default_value="ezrassor"
-                        ),
-                        execution_type_launch_arg = DeclareLaunchArgument(
-                            {"execution_type": LaunchConfiguration("execution_type")}
-                        )
-                    )
-                ]
-            )
+    #             # Unexpected argument
+    #             # parameters=[{
+    #             # "capabilities": LaunchConfiguration('capabilities'),
+    #             # "disable_capabilities": LaunchConfiguration('disable_capabilities'),
+    #             # }],
+                
+    #         )
+    #     ]
+    # )
 
-    if LaunchConfiguration("allow_trajectory_execution") == "true":
-        sensor_manager_launch_include = GroupAction(
-            actions=[
-                # push-ros-namespace to set namespace of included nodes
-                PushRosNamespace('move_group'),
-                IncludeLaunchDescription(
-                    PythonLaunchDescriptionSource(
-                        os.path.join(
-                            get_package_share_directory('ezrassor_arm_moveit_ros2'),
-                            'launch/sensor_manager.launch.xml')
-                    ),
-                    moveit_sensor_manager_launch_arg = DeclareLaunchArgument(
-                        "moveit_sensor_manager",
-                        default_value="ezrassor"
-                    )
-                )
-            ]
-        )
+    # if LaunchConfiguration("allow_trajectory_execution") == "true":
+    #     if LaunchConfiguration("fake_execution") == "true":
+    #         trajectory_execution_launch_include = GroupAction(
+    #             actions=[
+    #                 # push-ros-namespace to set namespace of included nodes
+    #                 PushRosNamespace('move_group'),
+    #                 IncludeLaunchDescription(
+    #                     PythonLaunchDescriptionSource(
+    #                         os.path.join(
+    #                             get_package_share_directory('ezrassor_arm_moveit_ros2'),
+    #                             'launch/sensor_manager.launch.xml')
+    #                     ),
+    #                     launch_arguments=[{
+    #                         "moveit_manage_controllers": LaunchConfiguration("true"),
+    #                         "moveit_controller_manager": LaunchConfiguration('fake'),
+    #                         "execution_type": LaunchConfiguration('execution_type')
+    #                     }]
+    #                 )
+    #             ]
+    #         )
+    #     else:
+    #         trajectory_execution_launch_include = GroupAction(
+    #             actions=[
+    #                 # push-ros-namespace to set namespace of included nodes
+    #                 PushRosNamespace('move_group'),
+    #                 IncludeLaunchDescription(
+    #                     PythonLaunchDescriptionSource(
+    #                         os.path.join(
+    #                             get_package_share_directory('ezrassor_arm_moveit_ros2'),
+    #                             'launch/sensor_manager.launch.xml')
+    #                     ),
+    #                     launch_arguments=[{
+    #                         "moveit_manage_controllers": LaunchConfiguration("true"),
+    #                         "moveit_controller_manager": LaunchConfiguration('ezrassor'),
+    #                         "execution_type": LaunchConfiguration('execution_type')
+    #                     }]
+    #                 )
+    #             ]
+    #         )
 
-        move_group_node = Node(
-            package='moveit_ros_move_group',
-            executable='move_group',
-            name='move_group',
-            launch_prefix_launch_arg='launch_prefix',
-            respawn='false',
-            output='screen',
-            arguments=LaunchConfiguration("command_args"),
-            #env=
-            parameters=[{
-                "allow_trajectory_execution": LaunchConfiguration('allow_trajectory_execution'),
-                "max_safe_path_cost": LaunchConfiguration('max_safe_path_cost'),
-                "jiggle_fraction": LaunchConfiguration('jiggle_fraction'),
-                "planning_scene_monitor/publish_planning_scene": LaunchConfiguration('publish_monitored_planning_scene'),
-                "planning_scene_monitor/publish_geometry_updates": LaunchConfiguration('publish_monitored_planning_scene'),
-                "planning_scene_monitor/publish_state_updates": LaunchConfiguration('publish_monitored_planning_scene'),
-                "planning_scene_monitor/publish_transforms_updates": LaunchConfiguration('publish_monitored_planning_scene'),
-            }]
-        )    
+    # if LaunchConfiguration("allow_trajectory_execution") == "true":
+    #     sensor_manager_launch_include = GroupAction(
+    #         actions=[
+    #             # push-ros-namespace to set namespace of included nodes
+    #             PushRosNamespace('move_group'),
+    #             IncludeLaunchDescription(
+    #                 PythonLaunchDescriptionSource(
+    #                     os.path.join(
+    #                         get_package_share_directory('ezrassor_arm_moveit_ros2'),
+    #                         'launch/sensor_manager.launch.xml')
+    #                 ),
+    #                 launch_arguments=[{
+    #                     "moveit_sensor_manager": LaunchConfiguration("ezrassor"),
+    #                 }]
+    #             )
+    #         ]
+    #     )
+
+    move_group_node = Node(
+        package='moveit_ros_move_group',
+        executable='move_group',
+        name='move_group',
+        # launch_prefix_launch_arg=LaunchConfiguration("launch_prefix"),
+        respawn='false',
+        output='screen',
+        arguments=[{
+            LaunchConfiguration("command_args"),
+            LaunchConfiguration("launch_prefix")
+        }],
+        #env=
+        parameters=[{
+            "allow_trajectory_execution": LaunchConfiguration('allow_trajectory_execution'),
+            "max_safe_path_cost": LaunchConfiguration('max_safe_path_cost'),
+            "jiggle_fraction": LaunchConfiguration('jiggle_fraction'),
+            "planning_scene_monitor/publish_planning_scene": LaunchConfiguration('publish_monitored_planning_scene'),
+            "planning_scene_monitor/publish_geometry_updates": LaunchConfiguration('publish_monitored_planning_scene'),
+            "planning_scene_monitor/publish_state_updates": LaunchConfiguration('publish_monitored_planning_scene'),
+            "planning_scene_monitor/publish_transforms_updates": LaunchConfiguration('publish_monitored_planning_scene'),
+        }]
+    )    
 
     return LaunchDescription([
         debug_launch_arg,
         launch_prefix_launch_arg,
         info_launch_arg,
+        command_args_launch_arg,
         pipeline_launch_arg,
         allow_trajectory_execution_launch_arg,
         fake_execution_launch_arg,
@@ -224,9 +221,9 @@ def generate_launch_description():
         capabilities_launch_arg,
         disable_capabilities_launch_arg,
         load_robot_description_launch_arg,
-        planning_context_launch_include,
-        planning_pipeline_launch_include,
-        trajectory_execution_launch_include,
-        sensor_manager_launch_include,
+        # planning_context_launch_include,
+        # planning_pipeline_launch_include,
+        # trajectory_execution_launch_include,
+        # sensor_manager_launch_include,
         move_group_node,
     ])
