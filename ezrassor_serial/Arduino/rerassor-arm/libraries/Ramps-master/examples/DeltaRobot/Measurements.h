12adcf94cc31659a1a60d94c304fce1a42561944
Matheus Ferreira <74433879+MatheusF1010@users.noreply.github.com>
2022-09-23 14:53:22 -0400

    Add files via upload

---
+++ ezrassor_serial/Arduino/rerassor-arm/libraries/Ramps-master/examples/DeltaRobot/Measurements.h
@@ -0,0 +1,20 @@
+/*
+	Afmetingen van de deltarobot of -printer
+	Door Brecht Ooms
+*/
+
+#ifndef _Measurements_h
+#define _Measurements_h
+
+#define STEPS_MM 160 //Steps per mm
+
+#define BASE_SIDE 343
+#define TOWER_HEIGHT 610
+
+#define ARM_LENGHT 310
+
+#define PLATFORM_SIDE 42
+#define TOOL_OFFSET 100
+
+
+#endif
